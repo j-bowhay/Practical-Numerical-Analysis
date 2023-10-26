fc69a6d567f5b4612e54fb146bc6aa2229a115bd
Jake Bowhay <jb9.bowhay@gmail.com>
2023-10-26 22:13:37 +0100

    q2 done

--- src/pna/quad.py
+++ src/pna/quad.py
@@ -25,3 +25,29 @@ def guass_legendre(f, a, b, n):
     eigenvalues, eigenvectors = np.linalg.eigh(T)
     w = 2 * eigenvectors[0, :] ** 2
     return 0.5 * (b - a) * w @ f(0.5 * (b - a) * eigenvalues + 0.5 * (a + b))
+
+
+def _second_order_central(f, x, h=np.finfo(float).eps ** (1 / 3)):
+    return (f(x + h) - 2 * f(x) + f(x - h)) / h**2
+
+
+def adaptive_composite_trapezium(f, a, b, n0, tol):
+    x = np.linspace(a, b, n0)
+
+    while True:
+        # note it really should be max|f''| however here we just use f''(\bar{x})
+        # where
+        # if the intervals are small hopefully this isn't a terrible approximation
+        error_summand = (
+            np.diff(x) ** 3
+            * np.abs(_second_order_central(f, (x[1:] + x[:-1]) / 2))
+            / 12
+        )
+        err = np.sum(error_summand)
+        if err < tol:
+            break
+        i = np.argmax(error_summand)
+        x = np.insert(x, i + 1, 0.5 * (x[i] + x[i + 1]))
+
+    h = np.diff(x)
+    return np.sum(0.5 * h * (f(x[:-1]) + f(x[1:])))
